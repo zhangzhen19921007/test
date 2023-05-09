fd0a16640d0f0ec59cbac710f20c419d5fe44d3a
Administrator <405364919@qq.com>
2023-05-09 16:33:46 +0800

    修改为test11111

---
+++ 基础概念/eval.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/9 20:45
+# @Author  : ZZ
+# @FileName: eval.py
+# @Software: PyCharm
+"""
+eval()函数，可以执行字符串表达式，并且返回结果,test111111
+"""
+x=int(input("请输入X的值："))
+r1=eval("x**3")
+print("x**3=",r1)
